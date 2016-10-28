20313e26ac2b52fe563319f903d1cb58dd395c98
nostromo <nostromo_@hotmail.it>
2016-10-28 11:11:28 +0200

    Add project files.

---
+++ Corso.Bologna/Corso.Bologna/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Corso.Bologna.Models;
+using Corso.Bologna.Services;
+using GalaSoft.MvvmLight;
+
+namespace Corso.Bologna.ViewModels
+{
+    public class MainViewModel: ViewModelBase
+    {
+        private IList<Recipe> _recipes;
+
+        public MainViewModel()
+        {
+            Title = nameof(MainViewModel);
+            LoadDataAsync();
+        }
+
+        public async void LoadDataAsync()
+        {
+            Recipes = await new RecipeService().GetRecipeAsync();
+        }
+        public string Title { get; set; }
+
+        public IList<Recipe> Recipes
+        {
+            get { return _recipes; }
+            set
+            {
+                       
+                Set(ref _recipes, value);
+            }
+        }
+    }
+}
---
+++ Corso.Bologna/Corso.Bologna/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Corso.Bologna.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<MainViewModel>();
+
+        }
+
+        public MainViewModel MainViewModel
+        {
+            get
+            {
+
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+  
+    }
+
+}
